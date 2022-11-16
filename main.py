203b95522072aff51a9b1a6210e58da8d0b9a93d
Nkasi-e <nkasijnr@gmail.com>
2022-11-16 16:04:34 +0100

    Added more methods

---
+++ main.py
@@ -0,0 +1,24 @@
+from item import Item
+from phone import Phone
+
+
+
+# Item.instantiate_from_csv()
+# print(Item.all_items)
+
+item1 = Item('John Doe', 750)
+
+item1.apply_discount(0.2)
+
+print(item1.price)
+
+
+
+
+
+
+
+
+
+
+
